256947d8f0e4909517656842bd472b2c287138ba
badriiya <alhomaizi.badriyah@gmail.com>
2022-12-10 23:35:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 # write your code here
+person=[
+    {
+        'name':"badriya",
+        'age': 21,
+        'hobbies': ["sleeping", "eating", "swimming"]
+
+
+    }
+]
+name=person[0].get('name')
+print(name)
+
+age=person[0].get('age')
+print(age)
+
+person[0]['age']= 24
+print(person)
+
+person[0]['country']= 'kuwait'
+print(person)
+
+print(len(person[0]))
+
+person[0]['hobbies'].append("reading")
+print(person)
+
+def check_hobbies(person):
+    if len(person[0]['hobbies'])>=3:
+        print(f"WOW YOU ARE AMAZING")
+check_hobbies(person)
